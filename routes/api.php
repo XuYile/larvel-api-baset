258ec5ecac93163f36772afa0ceb4ad9c402b49d
vagrant <vagrant@homestead>
2020-05-26 06:34:41 +0000

    firt commit

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+
+Route::prefix('v1')
+    ->namespace('Api')
+    ->name('api.v1.')
+    ->group(function () {
+
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))
+            ->group(function () {
+                Route::get('version', function () {
+                    return 'this is version v1';
+                })->name('version');
+            });
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))
+            ->group(function () {
+
+            });
+    });
+
